377c23e1af191aa2f21111b76e7179b378b01b28
Eduardo de Matos Almeida <eduardomatosalmeida@gmail.com>
2019-11-10 05:05:29 -0300

    Desenvolvimento de Aplicação Angular - Consumindo API Rest

---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,18 @@
+import { Restaurant } from "./restaurant/restaurant.model";
+import { MEAT_API } from "../app.api";
+import { Observable } from "rxjs";
+import { Http } from "@angular/http";
+import { Injectable } from "@angular/core";
+
+import "rxjs/add/operator/map";
+
+@Injectable()
+export class RestaurantsService {
+  constructor(private http: Http) {}
+
+  restaurants(): Observable<Restaurant[]> {
+    return this.http
+      .get(`${MEAT_API}/restaurants`)
+      .map(response => response.json());
+  }
+}
