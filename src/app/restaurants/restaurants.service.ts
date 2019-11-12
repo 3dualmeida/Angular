207669ad8413c1b86f8ec4fc0c4aeef77611d3e0
Eduardo de Matos Almeida <eduardomatosalmeida@gmail.com>
2019-11-12 03:47:11 -0300

    Desenvolvimento de Aplicação Angular - Consumindo API Rest com endpoits para id

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,10 +1,13 @@
-import { Restaurant } from "./restaurant/restaurant.model";
 import { MEAT_API } from "../app.api";
-import { Observable } from "rxjs";
 import { Http } from "@angular/http";
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs/Observable';
+import { ErrorHandler } from '../app.error.handler';
 
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+
+import { Restaurant } from "./restaurant/restaurant.model";
 
 @Injectable()
 export class RestaurantsService {
@@ -13,6 +16,13 @@ export class RestaurantsService {
   restaurants(): Observable<Restaurant[]> {
     return this.http
       .get(`${MEAT_API}/restaurants`)
-      .map(response => response.json());
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handlerError);
   }
 }
